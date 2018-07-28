5df2c2caadb97fed06d5eeb482c5a92feb3765c3
kostyan196 <40043789+kostyan196@users.noreply.github.com>
2018-07-28 14:16:16 +0700

    Добавьте файлы проекта.

---
+++ BinaryTree.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BinaryTrees
+{
+    public class BinaryTree<T> : IEnumerable<T>
+        where T : IComparable
+    {
+        class Node
+        {
+            public T Value { get; set; }
+            public Node Parent { get; set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+            public int LeftSize { get; set; }
+            public Node(T value)
+            {
+                Value = value;
+            }
+        }
+
+        Node Root { get; set; }
+        public int Count { get; private set; }
+
+        public T this[int index]
+        {
+            get { return GetIndex(index); }
+        }
+
+        private T GetIndex(int index)
+        {
+            if (Root == null || index > Count - 1 || index < 0)
+                throw new IndexOutOfRangeException();
+            if (Root.LeftSize == index) return Root.Value;
+            return GetIndex(index, Root);
+        }
+
+        private T GetIndex(int index, Node root)
+        {
+            int tempIndex = 0;
+            while (true)
+            {
+                if (root.LeftSize + tempIndex > index)
+                {
+                    root = root.Left;
+                }
+                else if (tempIndex + root.LeftSize == index)
+                    return root.Value;
+                else
+                {
+                    tempIndex += root.LeftSize + 1;
+                    root = root.Right;
+                }
+            }
+        }
+
+        public void Add(T value)
+        {
+            if (Root == null)
+            {
+                Root = new Node(value);
+                Count++;
+                return;
+            }
+            Add(value, Root);
+        }
+
+        private void Add(T value, Node root)
+        {
+            while (true)
+            {
+                if (value.CompareTo(root.Value) < 0)
+                {
+                    root.LeftSize++;
+                    if (root.Left == null)
+                    {
+                        var tempNode = new Node(value);
+                        tempNode.Parent = root;
+                        root.Left = tempNode;
+                        Count++;
+                        return;
+                    }
+                    root = root.Left;
+                }
+                else
+                {
+                    if (root.Right == null)
+                    {
+                        var tempNode = new Node(value);
+                        tempNode.Parent = root;
+                        root.Right = tempNode;
+                        Count++;
+                        return;
+                    }
+                    root = root.Right;
+                }
+            }
+        }
+
+        public bool Contains(T value)
+        {
+            if (Root == null) return false;
+            if (value.CompareTo(Root.Value) == 0)
+                return true;
+            return Contains(value, Root);
+        }
+
+        private bool Contains(T value, Node root)
+        {
+            while (true)
+            {
+                if (value.CompareTo(root.Value) == 0)
+                    return true;
+                else if (value.CompareTo(root.Value) < 0)
+                {
+                    if (root.Left == null)
+                        return false;
+                    root = root.Left;
+                }
+                else
+                {
+                    if (root.Right == null)
+                        return false;
+                    root = root.Right;
+                }
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            if (Root == null) return null;
+            return InOrder(Root).GetEnumerator();
+        }
+
+        private IEnumerable<T> InOrder(Node root)
+        {
+            int count = 0;
+            var visited = new HashSet<Node>();
+            while (count != Count)
+            {
+                while (root.Left != null && !visited.Contains(root.Left))
+                    root = root.Left;
+                if (!visited.Contains(root))
+                {
+                    yield return root.Value;
+                    visited.Add(root);
+                    count++;
+                }
+
+                if (root.Right != null && !visited.Contains(root.Right))
+                    root = root.Right;
+                else if (root.Parent != null) root = root.Parent;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ BinaryTreeInTask3_should.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTrees
+{
+    [TestFixture]
+    public class BinaryTreeInTask3_should
+    {
+        //Эта черная магия нужна, чтобы код тестов, которые используют индексацию, компилировался 
+        //при отсутствии индексатора в вашем классе.
+        //Совсем скоро вы и сами научитесь писать что-то подобное.
+
+        public static PropertyInfo GetIndexer<T>(BinaryTree<T> t)
+            where T : IComparable
+        {
+            return t.GetType()
+                .GetProperties()
+                .Select(z => new { prop = z, ind = z.GetIndexParameters() })
+                .Where(z => z.ind.Length == 1 && z.ind[0].ParameterType == typeof(int))
+                .SingleOrDefault()
+                ?.prop;
+        }
+
+        public static Func<int, T> MakeAccessor<T>(BinaryTree<T> tree)
+            where T : IComparable
+        {
+            var prop = GetIndexer(tree);
+            var param = Expression.Parameter(typeof(int));
+            return Expression.Lambda<Func<int, T>>(
+                Expression.MakeIndex(Expression.Constant(tree), prop, new[] { param }),
+                param)
+                .Compile();
+        }
+
+        public void Test<T>(IEnumerable<T> _values)
+            where T : IComparable
+        {
+            var values = _values.Shuffle();
+            var tree = new BinaryTree<T>();
+            if (GetIndexer(tree) == null)
+                Assert.Fail("Your BinaryTree does not implement indexing");
+            foreach(var e in values)
+                tree.Add(e);
+
+            var orderedValues = values.OrderBy(z => z).ToList();
+            var indexer = MakeAccessor(tree);
+            for (int i = 0; i < values.Count; i++)
+                Assert.AreEqual(orderedValues[i], indexer(i));
+        }
+
+        [Test]
+        public void SupportIndexers()
+        {
+            Test(Enumerable.Range(1, 30));
+        }
+
+    }
+}
